55412e4f2079bdbdeba46e0adf53e467ce9eb86b
Christina Wang <christinawang@fb.com>
2022-07-20 18:06:44 -0700

    [MAPS] added direction services

--- food-waste-ui/src/components/Map/Map.jsx
+++ food-waste-ui/src/components/Map/Map.jsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import {
   useJsApiLoader,
   GoogleMap,
-  useLoadScript,
   Marker,
+  DirectionsRenderer,
 } from "@react-google-maps/api";
+import { useState, useRef } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { Pane, Dialog, ListItem } from "evergreen-ui";
@@ -14,6 +15,8 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import "./Map.css";
+import { IconButton } from "@mui/material";
+
 ("use strict");
 
 export default function Map({
@@ -22,21 +25,39 @@ export default function Map({
   getLocation,
   coordinates,
 }) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = async () => {
+    setOpen(true);
+    await setOrigin(
+      new google.maps.LatLng(currentUser.location[0], currentUser.location[1])
+    );
+    await setDestination(
+      new google.maps.LatLng(
+        curProduct.current.props.position.lat,
+        curProduct.current.props.position.lng
+      )
+    );
+  };
   const handleClose = () => setOpen(false);
   const center = { lat: currentUser.location[0], lng: currentUser.location[1] };
 
   const containerStyle = {
     width: `90%`,
-    height: `1000px`,
+    height: `800px`,
   };
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
   });
 
-  const [map, setMap] = React.useState(null);
+  const [map, setMap] = useState(null);
+  const [directions, setDirections] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+  const [origin, setOrigin] = useState();
+  const [destination, setDestination] = useState();
+  let curProduct = useRef();
 
   const onLoad = React.useCallback(function callback(map) {
     map.setZoom(11);
@@ -47,17 +68,64 @@ export default function Map({
     setMap(null);
   }, []);
 
-  return !isLoaded ? (
-    <Skeleton />
-  ) : (
+  const calculateRoute = async () => {
+    if (origin == "" || !destination) {
+      return;
+    }
+    const directionService = new google.maps.DirectionsService();
+    console.log("destination in service", destination);
+    const results = await directionService.route({
+      origin: origin,
+      destination: destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirections(results);
+    setDistance(results.routes[0].legs[0].distance.text); //text returns readable value
+    setDuration(results.routes[0].legs[0].duration.text);
+  };
+
+  const clearRoute = () => {
+    setDirections(null);
+    setDistance("");
+    setDuration("");
+    setOrigin(currentUser.location);
+    setDestination([]);
+  };
+
+  if (!isLoaded) {
+    return <h1>Is Loading...</h1>;
+  }
+
+  return (
     <>
       <div
         style={{ position: "relative", overflow: "visible" }}
         className="map-style"
       >
         <div className="map-box">
-          <input type="text" placeholder="Origin" />
-          <input type="text" placeholder="Product Location" />
+          <input
+            type="text"
+            placeholder="Origin"
+            onChange={setOrigin}
+            defaultValue={origin}
+          />
+
+          <input
+            type="text"
+            placeholder="Product Location"
+            defaultValue={destination}
+          />
+
+          <Button onClick={calculateRoute}>Calculate Route</Button>
+          <IconButton
+            aria-label="center-back"
+            children={<FaLocationArrow />}
+            onClick={() => map.panTo(center)}
+          />
+          <div className="mapbox-results">
+            <h1>Distance: {distance}</h1>
+            <h1>Duration: {duration}</h1>
+          </div>
         </div>
         <GoogleMap
           zoom={2}
@@ -66,28 +134,22 @@ export default function Map({
           onLoad={onLoad}
           onUnmount={onUnmount}
         >
-          {products.map((item) => {
-            let location = { lat: item.location[0], lng: item.location[1] };
-            return (
-              <>
-                <Marker
-                  onClick={handleOpen}
-                  key={item.objectId}
-                  position={location}
-                ></Marker>
-                <Modal open={open} onClose={handleClose}>
-                  <Box className="modal-container">
-                    <Typography className="modal-modal-title">
-                      {item.name}
-                    </Typography>
-                    <Typography className="modal-modal-description">
-                      {item.description}
-                    </Typography>
-                  </Box>
-                </Modal>
-              </>
-            );
-          })}
+          <div className="markers">
+            {products.map((item) => {
+              let location = { lat: item.location[0], lng: item.location[1] };
+              return (
+                <>
+                  <Marker
+                    onClick={handleOpen}
+                    key={item.objectId}
+                    position={location}
+                    ref={curProduct}
+                  ></Marker>
+                  {directions && <DirectionsRenderer directions={directions} />}
+                </>
+              );
+            })}
+          </div>
         </GoogleMap>
       </div>
     </>
